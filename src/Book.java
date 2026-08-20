6b3f405cd05baea913a516083482d2ee9ec35b72
Nayudu Abhishek <naiduabhishek006@gmail.com>
2026-08-20 22:15:47 +0530

    The book class were added

---
+++ src/Book.java
@@ -0,0 +1,21 @@
+import com.sun.tools.javac.Main;
+
+import java.util.Scanner;
+
+class Book {
+    String title;
+    String Author;
+    void displayinfo(){
+        System.out.println(title+"by"+Author);
+    }
+
+    public static void main(String[] args) {
+        Book b = new Book();
+        Scanner sc = new  Scanner(System.in);
+        b.title = sc.nextLine();
+        b.Author = sc.nextLine();
+        b.displayinfo();
+
+    }
+}
+
